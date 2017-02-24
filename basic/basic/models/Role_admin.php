4f753266960fe507c29b9478fffa37c191ce7a16
lqy <goodlqy@163.com>
2017-02-24 20:21:54 +0800

    rbac wan san

--- basic/basic/models/Role_admin.php
+++ basic/basic/models/Role_admin.php
@@ -31,5 +31,18 @@ class Role_admin extends ActiveRecord{
         return array_column($data,'role_id');
     }
 
+    /*
+     * 查询用户拥有的权限
+     * */
+    public function roleAll($admin_id)
+    {
+        $roleAll = $this->find()->where(['admin_id'=>$admin_id])->asArray()->all();
+        $role_id = array_column($roleAll,'role_id');    //获取用户拥有的角色ID
+        $node_id = Role_node::find()->where(['IN','role_id',$role_id])->asArray()->all();
+        $node_id = array_column($node_id,'node_id');    //获取用户拥有的权限ID
+        $nodeAll = Node::find()->where(['IN','node_id',$node_id])->asArray()->all();
+        return array_column($nodeAll,'co_ac');
+    }
+
 
 }
