71c3b99bebf600fb45ce34e6ac988b1d95181215
Your Name <you@example.com>
2017-02-27 09:55:11 +0800

    login

--- basic/basic/controllers/home/IndexController.php
+++ basic/basic/controllers/home/IndexController.php
@@ -9,7 +9,7 @@ use yii\filters\VerbFilter;
 use app\models\Nav;  //模型层
 
 
-class IndexController extends Controller
+class IndexController extends CommonController
 {
     //前台公共视图
     public  $layout = '/proscenium';
@@ -18,13 +18,6 @@ class IndexController extends Controller
      */
     public function actionIndex()
     {
-        $nav_model = new Nav();
-        $info = json_decode(file_get_contents('systemConfig.txt'),true);
-        $nav_data = $nav_model->recursion();
-        $view = YII::$app->view;
-        $view->params['system'] = $info;
-        $view->params['nav'] =    array_slice($nav_data,0,14);
-;
         return $this->render('index');
     }
 
