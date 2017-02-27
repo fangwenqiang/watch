8e4fc77fcffe82d5c043dc1e7afdf2ee710ea539
gongyang <gongyang1217@qq.com>
2017-02-27 16:03:50 +0800

    gongyang

--- basic/basic/views/admin/goods/_form.php
+++ basic/basic/views/admin/goods/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'type_id')->textInput() ?>
+    <?= $form->field($model, 'gt_id')->textInput() ?>
 
     <?= $form->field($model, 'goods_sn')->textInput(['maxlength' => true]) ?>
 
