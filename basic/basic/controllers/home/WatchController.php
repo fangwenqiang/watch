407a4a75cdf174907431cebd9e261cd90ed85500
lqy <goodlqy@163.com>
2017-02-28 11:47:35 +0800

    wansan

--- basic/basic/controllers/home/WatchController.php
+++ basic/basic/controllers/home/WatchController.php
@@ -16,6 +16,7 @@ use yii\web\Controller;
 class WatchController extends CommonController{
 
     public $layout = '/proscenium';
+    public $onePage = '6';  //每页显示条数
 
     /*
      * 男士手表展示
@@ -23,31 +24,13 @@ class WatchController extends CommonController{
     public function actionBoylist()
     {
         $goods = new Goods();   //商品
-        $request = \Yii::$app->request;
-        $brand_id = $request->get('brand_id');
-
-        if($request->isPost){
-            //根据搜索框查询品牌下的所有男士手表
-            $brandName = $request->post('brand_name');
-            $goodsList = $this->brandname($brandName,'2');   //根据品牌名称查询商品
-            if(empty($goodsList)){
-                $data['goodsList'] = '1';
-            }else{
-                $data['goodsList'] = $goodsList;
-            }
-        }elseif(empty($brand_id)){
-            //查询所有男士手表
-            $data['goodsList'] = $goods->showType('2');
-        }else{
-            //根据品牌ID查询所有男士手表
-            $goodsList = $goods->showBrand('brand_id',$brand_id,'2');
-            if(empty($goodsList)){
-                $data['goodsList'] = '1';
-            }else{
-                $data['goodsList'] = $goodsList;
-            }
-        }
+        $order = 'ASC';
+        $data['goodsList'] = $goods->showType('2',$order);
         $data['brandList'] = $this->BrandAll(); //查询品牌
+
+        $count = count($data['goodsList']);
+        $data['pageStr'] = $this->pageStr($count,$this->onePage);
+
         return $this->render('boy',['data'=>$data]);
     }
 
@@ -75,11 +58,97 @@ class WatchController extends CommonController{
 
 
     /*
-     * 根据品牌名称查询商品
+     * 搜索 排序
      * */
-    public function brandname($brandName,$gt_id)
+    public function actionSearch()
     {
-        $brand = new Brand();
-        return $brand->brandGoods($brandName,$gt_id);    //获取到品牌下的所有商品
+        $goods = new Goods();   //商品
+        $brand = new Brand();   //品牌
+        $request = \Yii::$app->request;
+        $data = $request->get();
+        $order = 'DESC';
+
+        if(!empty($data['brand_name'])){
+             $dataBrand = $brand->brandId($data['brand_name']);
+            if(empty($dataBrand)){
+                die(json_encode(array('res'=>'1','msg'=>'暂时没有该相似品牌')));
+            }else{
+                $data['brand_id'] = $dataBrand['brand_id'];
+            }
+        }
+
+        // 价格排序
+        if (!empty($data['price_or']) && $data['price_or'] == 0) {
+            $orderField = 'shop_price';
+            $order = 'DESC';
+        }elseif(!empty($data['price_or']) && $data['price_or'] == 1){
+            $orderField = 'shop_price';
+            $order = 'ASC';
+        }
+
+        //销量
+        if (!empty($data['hot_or']) && $data['hot_or'] == 0) {
+            $orderField = 'click_count';
+            $order = 'DESC';
+        }elseif(!empty($data['hot_or']) && $data['hot_or'] == 1){
+            $orderField = 'click_count';
+            $order = 'ASC';
+        }
+
+        // 新品
+        if (!empty($data['new_or']) && $data['new_or'] == 0) {
+            $orderField = 'add_time';
+            $order = 'DESC';
+        }elseif(!empty($data['new_or']) && $data['new_or'] == 1){
+            $orderField = 'add_time';
+            $order = 'ASC';
+        }
+
+        $goodsList = $goods->showBrand('brand_id',$data['brand_id'],'2',$order,$orderField = '');
+        if(empty($goodsList)){
+            $goodsList = array('res'=>'1','msg'=>'该品牌下暂时没有商品');
+        }
+        die(json_encode($goodsList));
     }
+
+
+    /*
+     * 分页
+     * */
+    function pageStr($count,$onePage,$p= 1)
+    {
+        //总页数
+        $sunPage = ceil($count/$onePage) ;
+        //第一个
+        $fistPage = '1';
+        //上一页
+        $upPage = $p - 1 < 1 ? 1 : $p - 1;
+        //下一页
+        $nextPage = $p + 1 > $sunPage ? $sunPage : $p + 1;
+
+//                <div id="page_nav">
+//                    <span class="pre">上一页</span>&nbsp;<span class="cur">1</span><a  href="html" class="next">下一页</a>&nbsp;
+//                </div>
+//
+//                <div id="record">
+//        共有&nbsp;774&nbsp;页，跳到第&nbsp;<input id="jtp" type="text" class="txt" maxlength="4" maxpage="774" value="" />&nbsp;页&nbsp;<a id="btnJtp" href="javascript:void(0);" class="btn">确定</a>
+//                </div>
+
+
+        $pageStr = '';
+        $pageStr .='<div id="page_nav"><span class="pre"><a href="javascript:page('.$upPage.')">上一页</a></span>&nbsp;';
+        $pageStr .='<a href="javascript:page('.$nextPage.')">下一页</a>&nbsp;</div>';
+
+//        $pageStr .='共 '.$sunPage.' 页';
+//        $pageStr .='当前页第 '.$p.' 页';
+//        $pageStr .='每页 <input type="text" value="'.$onePage.'" class="onePage" alt="'.$p.'">';
+//        $pageStr .='<a href="javascript:page('.$fistPage.')">第一页</a> ';
+//        $pageStr .='<a href="javascript:page('.$upPage.')">上一页</a> ';
+//        $pageStr .='<a href="javascript:page('.$nextPage.')">下一页</a> ';
+//        $pageStr .='<a href="javascript:page('.$sunPage.')">末一页</a>';
+
+        return $pageStr;
+    }
+
+
 }
--- basic/basic/views/home/watch/boy.php
+++ basic/basic/views/home/watch/boy.php
@@ -14,21 +14,17 @@ use yii\helpers\Url;
         <a href="" rel="nofollow">首页</a>
         <i>&gt;</i>
         <h1><a href="">男士手表</a></h1>    </div>
-
     <div class="clearfix"></div>
-
     <div style="margin-top:25px;"></div>
     <div class="list_banner">
         <a href="http://data.wbiao.cn/ad.php?ad_id=1138"   rel="nofollow" onclick="_gaq.push(['_trackEvent','quan-zhan','heng-fu__1','http://data.wbiao.cn/ad.php?ad_id=1138']);"><img   src="Images/blank.gif" class="lazy" data-original="http://img2.wbiao.cn/ad/201403/21/139536510192630.jpg" alt="招行特惠" /></a>
     </div>
     <div style="clear:both">&nbsp;</div>    <!--banner结束-->
     <div class="rightArea w995">
-
         <div class="sift_tips">
             <div class="st_top">筛选结果</div>
             <div class="st_bot">找到&nbsp;“男士手表”&nbsp;<b class="red">18570</b>件</div>
         </div>
-
         <div class="sift_results">
             <a href="javascript:location.href='/shoubiao.html';" rel="nofollow" title="取消选择“男表”">男表<i>×</i></a></div>
 <!--        根据品牌搜索查询框-->
@@ -41,15 +37,13 @@ use yii\helpers\Url;
                     <div class="f_right w920">
                         <dl class="s1 rel mb10">
                             <dt class="on"><a href="javascript:;" rel="nofollow">不限</a></dt>
-                            <form action="<?=url::to(['home/watch/boylist'])?>" method="post" id="search">
-                                <input type="text" id="brand_search" name="brand_name" class="sa_search" value="输入品牌名" title="输入品牌名" />
-                                <input type="hidden" name="_csrf" class="_csrf" value="<?=Yii::$app->request->getCsrfToken() ?>">
-                            </form>
+                                <input type="text" id="brand_search" class="sa_search" value="输入品牌名" title="输入品牌名" />
                             <a href="<?=url::to(['home/watch/boylist']) ?>" id="showAll" class="all_brands">显示全部品牌</a>
                         </dl>
                         <?php foreach($data['brandList']as $val){ ?>
-                        <dl id="level1" num="1" class="s2"><dt><a href="javascript:;" rel="nofollow"><?=$val['sort']?></a></dt>
-                            <dd><a href="<?=url::to(['home/watch/boylist','brand_id'=>$val['brand_id']]) ?>" title="百达翡丽手表 Patek Philippe"><?=$val['brand_name']?></a></dd>
+                        <dl id="level1" num="1" class="s2">
+                            <dt><a href="javascript:;" rel="nofollow"><?=$val['sort']?></a></dt>
+                            <dd><a href="javascript:void (0)"  brand_id="<?=$val['brand_id']?>"  title="<?=$val['brand_name']?>"><?=$val['brand_name']?></a></dd>
                         </dl>
                         <?php } ?>
                     </div>
@@ -64,24 +58,21 @@ use yii\helpers\Url;
             <div class="sa_top">
                 <dl class="order_by">
                     <dt>排序：</dt>
-                    <dd class="on"><a href="" rel="nofollow" title="点击排序">价格<i class="p_all"></i></a></dd>
-                    <dd ><a href="javascript:void(0);" class="down">热销</a></dd>
-                    <dd><a href="" rel="nofollow" title="点击排序">最新<i class=""></i></a></dd>
+                    <dd class="on"><a href="javascript:void (0)" name="price_or" brand_id="all" rel="nofollow" title="点击排序">价格<i class="p_all"></i></a></dd>
+                    <dd ><a href="javascript:void(0);" name="hot_or"  brand_id="all" class="down">热销</a></dd>
+                    <dd><a href="javascript:void(0);" rel="nofollow"  brand_id="all" name="new_or" title="点击排序">最新<i class=""></i></a></dd>
                 </dl>
             </div>
 <!--            end 排序-->
             <div class="sa_mid w930">
                 <div id="goods_list" class="goods_list w930">
 <!--                    商品展示-->
-                    <ul>
+                    <ul class="ul_goods">
                         <?php if($data['goodsList'] == '1'){ ?>
                         <span><h2>该品牌下暂时没有商品</h2></span>
                         <?php }else{ ?>
                         <?php foreach($data['goodsList'] as $val){ ?>
                         <li>
-                            <a href="michel-herbelin-g5561.html" title="赫柏林Classic Gents系列12443/S01"  >
-                                <!-- <i class="c__tMsk"></i> -->
-                                <i class="c__pTag"><span style="">招行特惠 立减200元</span></i>
                                 <div class="tImg"><img src="Images/<?=$val['g_img']?>"></div>
                                 <div class="tNm"><?=$val['goods_name'].'&nbsp;&nbsp;'.$val['keywords']?></div>
                                 <div class="tPrc">
@@ -94,19 +85,15 @@ use yii\helpers\Url;
                             <label class="compare-off" id="compare_5561">对比
                                 <input autocomplete="off" type="checkbox" id="product_5561" title="勾选加入表款对比" onclick="javascript:addToCompareTable('5561','赫柏林Classic Gents系列12443/S01','http://imgb13.wbiao.cn/201312/18/12443_S01_97254.jpg','/michel-herbelin-g5561.html','￥2950',this)">
                             </label>
-                            <input type="hidden" id="goods_id" value="5561">
                         </li>
                         <?php } ?>
                         <?php } ?>
                     </ul>
 <!--                    end 商品展示-->
                 </div>
-
             </div>
-
 <!--            分页按钮-->
             <div class="sa_bot w930">
-
                 <!-- 翻页 Begin -->
                 <div id="page_nav">
                     <span class="pre">上一页</span>&nbsp;<span class="cur">1</span>&nbsp;<a href="">2</a>&nbsp;<a href="">3</a>&nbsp;<a href="">4</a>&nbsp;<span>&hellip;</span>&nbsp;<a href="">389</a>&nbsp;<span>&hellip;</span>&nbsp;<a  href="">774</a>&nbsp;<a  href="html" class="next">下一页</a>&nbsp;
@@ -119,8 +106,7 @@ use yii\helpers\Url;
             </div>
 <!--            end 分页按钮-->
             <div class="sa_share w930">
-                <span class="share">
-									</span>
+                <span class="share"></span>
                 <div id="bdshare" class="bdshare_t bds_tools get-codes-bdshare">
                     <!-- 将此,标记放在您希望显示like按钮的位置 -->
                     <div class="bdlikebutton"></div>
@@ -128,19 +114,16 @@ use yii\helpers\Url;
                     <script id="bdlike_shell"></script>
                 </div>
             </div>
-
         </div>
-
     </div>
 
     <div id="hot_display_position"></div>
     <div class="leftArea w210">
-
         <div class="hot">
             <div class="h_top"><div class="h_tit">热销推荐</div></div>
             <!--热销系列展示 -->
             <div class="h_mid">
-                <a rel="nofollow" class="h_link" href=""  >
+                <a rel="nofollow" class="h_link" href="">
                     <img src="Images/blank.gif" class="lazy" data-original="Images/16753956_91142.jpg" alt="法国精致腕表品牌：赫柏林-Classic Gents系列 12443/S01 男士石英表" />
                     <ul>
                         <li style="margin-top:28px;"><b>赫柏林</b></li>
@@ -152,7 +135,6 @@ use yii\helpers\Url;
                 </a>
             </div>
 <!--            end 热销系列-->
-
         </div>
         <!-- Hot End -->
         <!-- Side Ad Begin -->
@@ -167,15 +149,77 @@ use yii\helpers\Url;
             </ul>
         </div>
         <!-- Side Ad End -->
-
     </div>
 
     <script src="js/jquery.js"></script>
     <script type="text/javascript">
         $(function () {
+            _csrf = $('._csrf').val();
+            //提交搜索框
             $('.sa_search').blur(function () {
-                $('#search').submit();
-            })
+                var obj = new Object();
+                obj['brand_name'] = $(this).val();
+                where(obj);
+            });
+
+            //排序
+            rel = '1';   //计数
+
+            //搜索 品牌
+            $('.s2 dd a').click(function () {
+                var obj = new Object();
+                var brand_id = $(this).attr('brand_id');
+                $('.order_by dd a').attr('brand_id',brand_id);  //把品牌ID存放到排序按钮中
+                obj['brand_id'] = brand_id;
+                where(obj)
+            });
+            $('.order_by dd a').click(function () {
+                var  objOrder = new Object();
+                var or_name = $(this).attr('name');
+                objOrder[or_name] = rel%2;
+                rel++;
+                objOrder['brand_id'] = $(this).attr('brand_id');
+                $(this).parent().addClass('on').siblings().removeClass();    //选中样式
+                where(objOrder);
+            });
+
+            //拼接值
+            function where(obj)
+            {
+                var  alt = '';
+                $.each(obj, function (k, v) {
+                    alt +=k+'='+v+'&';
+                });
+                search(alt);
+            }
+
+            /*
+             * 拼接条件 替换页面
+             * */
+            function search(alt){
+                $.ajax({
+                    type:'GET',
+                    url:'<?=url::to(['home/watch/search'])?>',
+                    data:alt,
+                    dataType:'json',
+                    success: function (msg) {
+                        var str = '';
+                        if(msg['res'] == '1'){
+                            str +='<span><h2>'+msg['msg']+'</h2></span>'
+                        }else{
+                            $.each(msg, function (k,v) {
+                                str += '<li><div class="tImg"><img src="Images/'+v.g_img+'"></div>';
+                                str += '<div class="tNm">'+ v.goods_name+'&nbsp;&nbsp;'+v.keywords+'</div><div class="tPrc">';
+                                str += '￥<span>'+v.shop_price+'</span><i>欧洲同步价</i></div>';
+                                str += '<div class="tInfo">销量<b>240</b></div></a>';
+                                str += '<label class="compare-off" id="compare_5561">对比';
+                                str +='<input autocomplete="off" type="checkbox" id="product_5561" title="勾选加入表款对比"></label></li>';
+                            })
+                        }
+                        $('.ul_goods').html(str);
+                    }
+                });
+            }
         })
 
     </script>
