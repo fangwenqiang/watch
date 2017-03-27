8c18ba95cd14cce3a69641187e5a34f34f392ac1
pjp <492653269@qq.com>
2017-03-27 08:40:02 +0800

    dasdadasdasdaasdadas

--- basic/basic/views/layouts/personal.php
+++ basic/basic/views/layouts/personal.php
@@ -100,7 +100,6 @@ use yii\helpers\Url;
 <link rel="stylesheet" href="css/index.css" type="text/css" media="screen, projection"/>
 <!--index banner start  ---------------------------------------------------------------------------------------------->
 <?= $content ?>
-<!--index banner end----------------------------------------------------------------------------------------------------->
 
 	<div class="leftArea">
         <div class="leftArea">
@@ -109,9 +108,9 @@ use yii\helpers\Url;
                 <div class="t"><i class="u__trade"></i><font class="f_fixed">交易管理</font></div>
                 <div class="c">
                     <ul>
-                        <li><i></i><a href="<?php echo Url::to(['home/personal/my_order'])?>" title="我的订单" rel="nofollow">我的订单 (<span class="cb01">1</span>)</a></li>
-                        <li><i></i><a href="<?php echo Url::to(['home/personal/my_periods'])?>" title="我的分期" rel="nofollow">我的分期 (<span class="cb01">1</span>)</a></li>
-                        <li><i></i><a href="<?php echo Url::to(['home/personal/my_presell'])?>" title="我的预售" rel="nofollow">我的预售 (<span class="cb01">0</span>)</a></li>
+                        <li><i></i><a href="<?php echo Url::to(['home/personal/my_order'])?>" title="我的订单" rel="nofollow">我的订单 </a></li>
+                        <li><i></i><a href="<?php echo Url::to(['home/personal/my_periods'])?>" title="我的分期" rel="nofollow">我的分期 </a></li>
+                        <li><i></i><a href="<?php echo Url::to(['home/personal/my_presell'])?>" title="我的预售" rel="nofollow">我的预售</a></li>
                         <li><i></i><a href="<?php echo Url::to(['home/personal/my_address'])?>" title="收货地址" rel="nofollow">收货地址</a></li>
                         <li><i></i><a href="<?php echo Url::to(['home/personal/gift_card'])?>" title="礼品卡" rel="nofollow">礼品卡</a></li>
                         <li style="border:0;"><i></i><a href="<?php echo Url::to(['home/personal/my_coupon'])?>" title="代金券/优惠券" rel="nofollow">代金券/优惠券</a></li>
